fae9aa28ef02189ea6f07f5f5561fda465a274c1
krogenth <kelleydaniel15@gmail.com>
2022-07-16 21:51:52 -0400

    add build script

---
+++ build.ps1
@@ -0,0 +1,9 @@
+msbuild /m /verbosity:detailed /p:Configuration=Debug /p:Platform=x64 ./vs/G2DataGUI.sln
+if(!$?) {
+    throw "Failed to build G2DataGUI Debug"
+}
+
+msbuild /m /verbosity:detailed /p:Configuration=Release /p:Platform=x64 ./vs/G2DataGUI.sln
+if(!$?) {
+    throw "Failed to build G2DataGUI Debug"
+}
