0fc716a4c22afc6471b2c918c445fc25157ed37a
krogenth <kelleydaniel15@gmail.com>
2023-01-08 01:31:33 -0500

    fix StartImGui function to always return value

--- src/ImGuiInstance.cpp
+++ src/ImGuiInstance.cpp
@@ -40,6 +40,7 @@ bool StartImGui() {
     // Setup Platform/Renderer bindings
     ImGui_ImplWin32_Init(hwnd);
     ImGui_ImplDX12_Init(g_pd3dDevice, NUM_FRAMES_IN_FLIGHT, DXGI_FORMAT_R8G8B8A8_UNORM, g_pd3dSrvDescHeap, g_pd3dSrvDescHeap->GetCPUDescriptorHandleForHeapStart(), g_pd3dSrvDescHeap->GetGPUDescriptorHandleForHeapStart());
+    return true;
 }
 
 void EndImGui() {
